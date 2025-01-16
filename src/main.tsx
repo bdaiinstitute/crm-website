a8185cec2602ae725692e509c16bd5a617806e0d
Giovanni Remigi <gremigi@theaiinstitute.com>
2025-01-16 14:07:55 +0100

    Importing tailwind directly.

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 
 import "./css/bulma.css";
+import "tailwindcss/tailwind.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
